6e40e3d6cdfe186b23f951a33d70c5f81ca92311
Patrick Werner <boon.werner@gmail.com>
2020-03-20 14:23:40 +0100

    issue #16: add mac default java path

--- resources/osx/start.sh
+++ resources/osx/start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+JAVA_DIR=
+if ! hash java 2>/dev/null; then
+  JAVA_DIR=/Library/Internet\ Plug-Ins/JavaAppletPlugin.plugin/Contents/Home/bin/
+fi
+
 cd "$(dirname $BASH_SOURCE)"
 cd ..
-java -jar ${artifactId}.jar
+"$JAVA_DIR"java -jar ${artifactId}.jar
